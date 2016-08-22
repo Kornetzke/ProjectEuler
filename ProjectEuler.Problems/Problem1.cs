440e223fe2f5b507074e7857a131618feaff0f1e
Kyle Kornetzke <kyle.kornetzke@gmail.com>
2016-08-21 23:14:04 -0500

    Created tests and solution for problem one

---
+++ ProjectEuler.Problems/Problem1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    /// Find the sum of all the multiples of 3 or 5 below 1000.
+    /// </summary>
+    public class Problem1
+    {
+        IEnumerable<int> multiplesToCheck;
+
+        /// <summary>
+        /// Standard 3 and 5 will be the multiples that are checked for
+        /// </summary>
+        public Problem1() : this(3,5)
+        {
+
+        }
+        
+
+        public Problem1(params int[] multiplesToCheck)
+        {
+            this.multiplesToCheck = new List<int>(multiplesToCheck);
+        }
+        public long FindSum(int upperBounds)
+        {
+            long sum = 0;
+            IEnumerable<long> multiples = FindMultiples(upperBounds);
+            foreach (long multiple in multiples)
+            {
+                sum += multiple;
+            }
+
+            return sum;
+        }
+
+        private IEnumerable<long> FindMultiples(int upperBounds)
+        {
+            List<long> multiples = new List<long>();
+            for (int i = 0; i < upperBounds; i++)
+            {
+                foreach (int multipleToCheck in multiplesToCheck)
+                {
+                    if (i % multipleToCheck == 0)
+                    {
+                        multiples.Add(i);
+                        //breaks out of multiples to check loop, this prevents duplicate multiples to be added
+                        break;
+
+                    }
+                }
+            }
+            return multiples;
+        }
+    }
+}
---
+++ ProjectEuler.Tests/Problem1Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ProjectEuler.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace ProjectEuler.Tests
+{
+    /// <summary>
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    /// Find the sum of all the multiples of 3 or 5 below 1000.
+    /// </summary>
+    [TestClass]
+    public class Problem1Test
+    {
+
+        Problem1 p1;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            p1 = new Problem1();
+        }
+
+        [TestMethod]
+        public void SampleProblemTest()
+        {
+            long expected = 23L;
+            long actual = p1.FindSum(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UpperBoundsZeroTest()
+        {
+            long expected = 0;
+            long actual = p1.FindSum(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Upperbounds12Test()
+        {
+            // natural number below 12 that are multiples of 3 or 5 summed together
+            long expected = 3 + 5 + 6 + 9 + 10; // 12 is not included, below does not include
+            long actual = p1.FindSum(12);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        //This test will stress numbers that are multiples of both 3 and 5, 15 shouldn't appear twice
+        [TestMethod]
+        public void UpperBounds16Test()
+        {
+            long expected = 3 + 5 + 6 + 9 + 10 + 12 + 15;
+            long actual = p1.FindSum(16);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UpperBounds1000Test()
+        {
+            long expected = 233168; //found from projecteuler.net problem1 solution
+            long actual = p1.FindSum(1000);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
