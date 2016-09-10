eaf0873a6132d46e6cdddc648516f098b690b53b
Kyle Kornetzke <kyle.kornetzke@gmail.com>
2016-09-10 17:42:54 -0500

    Added a simple console interface to problem3. Allows the user to input numbers to be used to calculate prime factors for and outputs to console.

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectEuler.Problems;
+
+namespace ConsoleInterface
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputString;
+            bool running = true;
+            do
+            {
+                Console.WriteLine("Input number to find prime factors");
+                inputString = Console.ReadLine();
+                long number;
+                try
+                {
+                    number = Int64.Parse(inputString);
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                long[] factors = Problem3.GetPrimeFactorsOf(number);
+                long largest = factors.Max();
+                StringBuilder sb = new StringBuilder(String.Format("Prime Factors for Number : {0}", number));
+                sb.AppendLine();
+                foreach(long i in factors)
+                {
+                    sb.Append(i + " ");
+                }
+                sb.AppendLine();
+                sb.AppendLine("Largest prime is " + largest);
+                Console.WriteLine(sb.ToString());
+
+            } while (running);
+        }
+
+    }
+}
