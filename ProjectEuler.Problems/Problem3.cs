c45cd87f13cc0434e909c9e925a81a4d864e888d
Kyle Kornetzke <kyle.kornetzke@gmail.com>
2016-09-10 15:53:47 -0500

    Added problem3 with tests. Added a better GetPrimeFactorsOf Method, now completes problem much quicker.

---
+++ ProjectEuler.Problems/Problem3.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    /// Largest prime factor
+    /// Problem 3
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    public static class Problem3
+    {
+        public static long FindLargestPrimeFactorOf(long value)
+        {
+            if (value == 0)
+                throw new ArithmeticException("Zero contains no prime factors");
+            long largestPrimeFactor = 0;
+
+            if (IsPrime(value))
+                largestPrimeFactor = value;
+            else
+            {
+
+                long[] primeFactors = GetPrimeFactorsOf(value);
+
+                if (primeFactors.Length != 0)
+                {
+                    foreach (int i in primeFactors)
+                    {
+                        if (i > largestPrimeFactor)
+                        {
+                            largestPrimeFactor = i;
+                        }
+                    }
+                }
+            }
+            
+
+            return largestPrimeFactor;
+        }
+
+        private static long[] GetPrimeFactorsOf(long value)
+        {
+            HashSet<long> primeFactors = new HashSet<long>();
+
+            for(long i =2; i <= value; i++)
+            {
+                
+                bool divisable = value % i == 0;
+                if (divisable)
+                {
+                    if (IsPrime(i))
+                    {
+                        primeFactors.Add(i);
+
+                        value = value / i;
+                        if (IsPrime(value))
+                        {
+                            primeFactors.Add(value);
+                            break;
+                        }
+                        
+                        i--;
+                    }else
+                    {
+                        throw new Exception("Woah");
+                    }
+                }
+            }
+
+            return primeFactors.ToArray<long>();
+
+        }
+
+        private static bool IsPrime(long value)
+        {
+            bool prime = true;
+            for(long i =2; i < value; i++)
+            {
+                if (value % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            return prime;
+        }
+    }
+}
---
+++ ProjectEuler.Tests/Problem3Test.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Problems;
+
+namespace ProjectEuler.Tests
+{
+    [TestClass]
+    public class Problem3Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void FindLargetPrimeFactorOfZero()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(0);
+            Assert.Fail("Arithmetic Exception not thrown");
+        }
+
+        [TestMethod]
+
+        public void FindLargestPrimeFactorOfOne()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(1);
+            long expected = 1;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void FindLargestPrimeFactorOfTwo()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(2);
+            long expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargestPrimeFactorOfThree()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(3);
+            long expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargestPrimeFactorOfFour()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(4);
+            long expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargestPrimeFactorOfFive()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(5);
+            long expected = 5;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FindLargestPrimeFactorOfSix()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(6);
+            long expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindLargestPrimeFactorOf12()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(12);
+            long expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindLargestPrimeFactorOf346896()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(346896);
+            long expected = 73;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindLargestPrimeFactorOf600851475143()
+        {
+            long actual = Problem3.FindLargestPrimeFactorOf(600851475143);
+            long expected = 6857;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
